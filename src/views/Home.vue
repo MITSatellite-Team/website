966c969e13d9bf74e5bd015969091b00216adbf3
outercloudstudio <outercloudstudio@gmail.com>
2026-05-15 22:10:52 -0400

    add logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,8 @@
 <script lang="ts" setup>
+
 </script>
 
 <template>
-    <nav>
-        <span>MIT Satellite</span>
-        
-        <div class="nav-links">
-            <RouterLink to="/projects">Projects</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink to="/wiki">Wiki</RouterLink>
-            <RouterLink to="/get-involved"><button class="nav-button">Get Involved</button></RouterLink>
-        </div>
-    </nav>
-
     <main>
         <div class="gradients">
             <div class="corner-gradient"></div>
@@ -33,44 +23,6 @@
 </template>
 
 <style scoped>
-nav {
-    position: sticky;
-
-    display: flex;
-
-    justify-content: space-between;
-    align-items: center;
-
-    height: 4rem;
-    margin-bottom: calc(-4rem - 20px);
-
-    padding: 10px 20px;
-
-    z-index: 1;
-}
-
-.nav-links {
-    display: flex;
-
-    gap: 60px;
-
-    align-items: center;
-}
-
-.nav-links > a {
-    text-decoration: none;
-}
-
-.nav-button {
-    background: none;
-
-    border: 1px solid var(--text);
-    color: var(--text);
-
-    font-size: 1rem;
-    font-weight: 400;
-}
-
 main {
     background-image: url('../assets/earth-hero.png');
     background-size: cover;
